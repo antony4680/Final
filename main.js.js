69386a839e6b7f3f82678d951fec5e09445425a4
antony4680 <83488126+antony4680@users.noreply.github.com>
2021-05-01 23:22:14 +0530

    Update main.js.js

--- main.js.js
+++ main.js.js
@@ -3,120 +3,114 @@ alert(
 );
 
 var c = document.getElementById("my-canvas");
-var cxt = c.getContext("2d");
+var ctx = c.getContext("2d");
 
-document.body.style.backgroundImage = "url('/Images/background.jpg')";
+document.body.style.backgroundImage = "url('https://cdn.glitch.com/03399de1-e0c0-46ae-8e57-848c7e2078c6%2Fbackground.jpg?v=1619890776529')";
 
-let loadImage = (src, callback) => {
-  let img = document.createElement("img");
-  img.onload = () => callback(img);
-  img.src = src;
-};
+var loadImage =(src, callback)=>{
+    var img= document.createElement('img');
+    img.onload=()=>callback(img);
+    img.src=src;
+}
 
-let imagePath = (frameNumber, animation) => {
-  return "D:/Project/" + animation + "/" + frameNumber + ".png";
-};
+var imagePath = (frameno, animation) => {
+    return "/images/" +animation + '/' + frameno + ".png";
+}
 
-let frames = {
-  idle: [1, 2, 3, 4, 5, 6, 7, 8],
-  kick: [1, 2, 3, 4, 5, 6, 7],
-  punch: [1, 2, 3, 4, 5, 6, 7],
-  forward: [1, 2, 3, 4, 5, 6],
-  backward: [1, 2, 3, 4, 5, 6],
-  block: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-};
+let frames = { idle:[1,2,3,4,5,6,7,8],
+    kick:[1,2,3,4,5,6,7],
+    punch:[1,2,3,4,5,6,7], 
+    backward:[1,2,3,4,5,6],
+    block:[1,2,3,4,5,6,7,8,9],
+    forward:[1,2,3,4,5,6]
+    };
 
-let loadImages = (callback) => {
-  // Calls back with an array of loaded images
-  let images = {
-    idle: [],
-    kick: [],
-    punch: [],
-    forward: [],
-    backward: [],
-    block: [],
-  };
-  let imagesToLoad = 0;
+let loadImages =(callback)=> {
+    let images = { idle:[], kick:[], punch:[], backward:[], block:[], forward:[]};
+    let imagesToLoad = 0;
 
-  ["idle", "kick", "punch", "forward", "backward", "block"].forEach(
-    (animation) => {
-      let animationFrames = frames[animation];
-      imagesToLoad = imagesToLoad + animationFrames.length;
+    ["idle", "kick", "punch", "backward", "block", "forward"].forEach((animation) =>{
+        let animateFrames = frames[animation];
+        imagesToLoad += animateFrames.length;
 
-      animationFrames.forEach((frameNumber) => {
-        let path = imagePath(frameNumber, animation);
-        loadImage(path, (image) => {
-          images[animation][frameNumber - 1] = image;
-          imagesToLoad = imagesToLoad - 1;
-
-          if (imagesToLoad === 0) {
-            callback(images);
-          }
+        animateFrames.forEach((frameno)=>{
+            let path = imagePath(frameno, animation);
+            loadImage(path, (image) => {
+                images[animation][frameno - 1] = image;
+                imagesToLoad -= 1;
+    
+                if(imagesToLoad === 0){
+                    callback(images);
+                }
+            });
         });
-      });
-    }
-  );
+       
+    });
 };
 
-let animate = (cxt, images, animation, callback) => {
-  images[animation].forEach((image, index) => {
-    setTimeout(() => {
-      cxt.clearRect(0, 0, 500, 500);
-      cxt.drawImage(image, 0, 0, 500, 500);
-    }, index * 100);
-  });
+let animate = (ctx, images, animation, callback) =>{
+    images[animation].forEach((image, index)=>{
+        setTimeout(()=>{
+            ctx.clearRect(0,0,500,500);
+            ctx.drawImage(image, 0, 0,500,500);
+        }, index*100);
+    });
 
-  setTimeout(callback, images[animation].length * 100);
-};
+    setTimeout(callback, images[animation].length * 100);
+}
 
-loadImages((images) => {
-  let queuedAnimation = [];
-  let aux = () => {
-    let selectedAnimation;
+loadImages((images, )=>{
+    let quedAnimation =[];
 
-    if (queuedAnimation.length === 0) {
-      selectedAnimation = "idle";
-    } else {
-      selectedAnimation = queuedAnimation.shift();
-    }
+    let aux =()=>{
+        let selectedAnimation;
 
-    animate(cxt, images, selectedAnimation, aux);
-  };
+        if(quedAnimation.length === 0){
+            selectedAnimation = "idle";
+        } else{
+            selectedAnimation = quedAnimation.shift();
+        }
 
-  aux();
+        animate(ctx, images, selectedAnimation, aux);
+    };
 
-  document.getElementById("kick").onclick = () => {
-    queuedAnimation.push("kick");
-  };
+    aux();
+   
+   document.getElementById("kick").onclick =()=>{
+       quedAnimation.push("kick");
+   }
 
-  document.getElementById("punch").onclick = () => {
-    queuedAnimation.push("punch");
-  };
+   document.getElementById("punch").onclick =()=>{
+       quedAnimation.push("punch");
+   }
 
-  document.getElementById("forward").onclick = () => {
-    queuedAnimation.push("forward");
-  };
+   document.getElementById("block").onclick =()=>{
+    quedAnimation.push("block");
+   }
 
-  document.getElementById("backward").onclick = () => {
-    queuedAnimation.push("backward");
-  };
+   document.getElementById("backward").onclick =()=>{
+    quedAnimation.push("backward");
+   }
 
-  document.getElementById("block").onclick = () => {
-    queuedAnimation.push("block");
-  };
+   document.getElementById("forward").onclick =()=>{
+    quedAnimation.push("forward");
+   }
 
-  document.addEventListener("keyup", (event) => {
-    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
-    if (key === "ArrowLeft") {
-      queuedAnimation.push("kick");
-    } else if (key === "ArrowRight") {
-      queuedAnimation.push("punch");
-    } else if (key === "ArrowUp") {
-      queuedAnimation.push("forward");
-    } else if (key === "ArrowDown") {
-      queuedAnimation.push("backward");
-    } else {
-      queuedAnimation.push("block");
-    }
-  });
+
+   document.addEventListener("keyup", (event)=>{
+       const key = event.key;
+       if(key==="ArrowLeft"){
+        quedAnimation.push("kick");
+       } else if(key === "ArrowRight"){
+        quedAnimation.push("punch");
+       } else if( key === 'ArrowUp'){
+        quedAnimation.push("forward"); 
+       } else if( key === "ArrowDown"){
+        quedAnimation.push("backward");
+       } else {
+         quedAnimation.push("block");
+       }
+   });
 });
+
+
